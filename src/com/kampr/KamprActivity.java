e84d8065950239bc14f80e1decc07375c4baaa32
Nitin Dhar <nitindhar7@yahoo.com>
2011-09-19 01:18:32 -0400

    added empty structure for kampr app

---
+++ src/com/kampr/KamprActivity.java
@@ -0,0 +1,13 @@
+package com.kampr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KamprActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
