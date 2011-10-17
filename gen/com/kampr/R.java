e2996b582a1902f4300d30aba30799befe78f8b1
Nitin Dhar <nitindhar7@yahoo.com>
2011-10-16 20:41:40 -0400

    created drawable to set gradient on action bar

--- gen/com/kampr/R.java
+++ gen/com/kampr/R.java
@@ -11,14 +11,15 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_tab_links=0x7f020000;
-        public static final int icon=0x7f020001;
-        public static final int posts_menu_logout=0x7f020002;
-        public static final int posts_menu_settings=0x7f020003;
-        public static final int tab_size_mdpi_codes=0x7f020004;
-        public static final int tab_size_mdpi_links=0x7f020005;
-        public static final int tab_size_mdpi_questions=0x7f020006;
-        public static final int tab_size_mdpi_snaps=0x7f020007;
+        public static final int action_bar=0x7f020000;
+        public static final int ic_tab_links=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int posts_menu_logout=0x7f020003;
+        public static final int posts_menu_settings=0x7f020004;
+        public static final int tab_size_mdpi_codes=0x7f020005;
+        public static final int tab_size_mdpi_links=0x7f020006;
+        public static final int tab_size_mdpi_questions=0x7f020007;
+        public static final int tab_size_mdpi_snaps=0x7f020008;
     }
     public static final class id {
         public static final int body=0x7f060018;
