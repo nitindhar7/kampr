6a081a7d47cd4bbcf10a85c2e0af53b4a525509c
Nitin Dhar <nitindhar7@yahoo.com>
2012-01-16 16:59:34 -0500

    redesigned post activity a with new post infobar and user infobars. added comments icon to actionbar

--- src/com/kampr/posts/PostActivity.java
+++ src/com/kampr/posts/PostActivity.java
@@ -32,6 +32,7 @@ import com.kampr.models.User;
 import com.kampr.runnables.SnapRunnable;
 import com.kampr.util.ImageUtils;
 import com.kampr.util.LayoutUtils;
+import com.kampr.util.SpanUtils;
 import com.kampr.util.TextUtils;
 
 public class PostActivity extends Activity implements OnClickListener {
@@ -48,23 +49,20 @@ public class PostActivity extends Activity implements OnClickListener {
     protected static Thread _fetchSnapThread;
     protected static SnapHandler _handler;
 
+    private RelativeLayout _infobar;
     protected JSONObject _postJSON;
     protected Thread _fetchPostThread;
     protected TextView _actionbarLogo;
     protected Bitmap _userIconBitmap;
-    protected TextView _postLikesCount;
     protected TextView _postLikes;
-    protected TextView _postViewsCount;
     protected TextView _postViews;
-    protected TextView _postCommentsCount;
-    protected TextView _postComments;
     protected TextView _postTitle;
     protected TextView _postUrl;
     protected TextView _postUsername;
     protected TextView _postDate;
     protected TextView _postDescription;
     protected TextView _postContent;
-    protected RelativeLayout _postViewComments;
+    protected TextView _postViewComments;
     protected ImageView _postUserIcon;
     protected ImageView _postOriginal;
 
@@ -73,38 +71,32 @@ public class PostActivity extends Activity implements OnClickListener {
         setContentView(R.layout.post);
         super.onCreate(savedInstanceState);
 
-        LayoutUtils.layoutOverride(findViewById(R.id.post_comments), View.GONE);
-        LayoutUtils.layoutOverride(findViewById(R.id.post_comments_count), View.GONE);
-
         _spinner = (ProgressBar) findViewById(R.id.actionbar_spinner);
         _actionbarLogo = (TextView) findViewById(R.id.actionbar_logo);
+        _infobar = (RelativeLayout) findViewById(R.id.post_infobar);
         _postTitle = (TextView) findViewById(R.id.post_title);
         _postUrl = (TextView) findViewById(R.id.post_url);
         _postUsername = (TextView) findViewById(R.id.post_user_name);
         _postDate = (TextView) findViewById(R.id.post_date);
         _postDescription = (TextView) findViewById(R.id.post_description);
         _postUserIcon = (ImageView) findViewById(R.id.user_icon_thumbnail);
-        _postLikes = (TextView) findViewById(R.id.post_likes);
-        _postViews = (TextView) findViewById(R.id.post_views);
-        _postLikesCount = (TextView) findViewById(R.id.post_likes_count);
-        _postViewsCount = (TextView) findViewById(R.id.post_views_count);
+        _postLikes = (TextView) findViewById(R.id.post_like_count_label);
+        _postViews = (TextView) findViewById(R.id.post_view_count_label);
         _postOriginal = (ImageView) findViewById(R.id.snap_large_url);
         _postContent = (TextView) findViewById(R.id.post_content);
-        _postViewComments = (RelativeLayout) findViewById(R.id.post_view_comments);
 
         _postOriginal.setOnClickListener(this);
-        _postUserIcon.setOnClickListener(this);
+        _infobar.setOnClickListener(this);
         
         _handler = new SnapHandler(this, _spinner, _postOriginal);
 
         _post = (Post) getIntent().getSerializableExtra("post");
 
-        if (_post.getCommentCount() == 0) {
-            LayoutUtils.layoutOverride(_postViewComments, View.GONE);
-        } else {
-            _postComments = (TextView) findViewById(R.id.post_view_comments_arrow);
-            _postCommentsCount = (TextView) findViewById(R.id.post_view_comments_count);
-            _postCommentsCount.setText(Integer.toString(_post.getCommentCount()));
+        if (_post.getCommentCount() > 0) {
+            _postViewComments = (TextView) findViewById(R.id.actionbar_comment);
+            
+            LayoutUtils.layoutOverride(_postViewComments, View.VISIBLE);
+
             _postViewComments.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -115,13 +107,13 @@ public class PostActivity extends Activity implements OnClickListener {
             });
         }
 
-        _postTitle.setText(_post.getTitle());
+        _postTitle.setText(_post.getTitle().toUpperCase());
         _postUsername.setText(_post.getUserName());
         _postDate.setText(_post.getCreatedAt());
         _postDescription.setText(TextUtils.cleanseText(_post.getDescription()));
         _postDescription.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
-        _postLikesCount.setText(Integer.toString(_post.getLikeCount()));
-        _postViewsCount.setText(Integer.toString(_post.getViewCount()));
+        _postLikes.setText(Integer.toString(_post.getLikeCount()));
+        _postViews.setText(Integer.toString(_post.getViewCount()));
         _userIconBitmap = ImageUtils.getBitmapFromByteArray(getIntent().getByteArrayExtra("post_user_icon"));
         _postUserIcon.setImageBitmap(_userIconBitmap);
 
@@ -153,6 +145,8 @@ public class PostActivity extends Activity implements OnClickListener {
             _postDescription.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
             LayoutUtils.layoutOverride(findViewById(R.id.actionbar_spinner), View.GONE);
         }
+        
+        SpanUtils.setFont(getApplicationContext(), _postTitle);
     }
 
     @Override
@@ -163,7 +157,7 @@ public class PostActivity extends Activity implements OnClickListener {
                 fullScreen.putExtra("snaps_original_url", _post.getSnap());
                 startActivity(fullScreen);
                 break;
-            case R.id.user_icon_thumbnail:
+            case R.id.post_infobar:
                 User user = _post.getUser();
                 Intent userIntent = new Intent(getApplicationContext(), UserActivity.class);
                 userIntent.putExtra("user", user);
