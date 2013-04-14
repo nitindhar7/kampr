ed2e2c94c01e6f130bea7d1409a12bf7e2b477bc
Nitin Dhar <nitindhar7@yahoo.com>
2013-04-14 00:52:51 -0400

    new logo, new login screen

--- src/com/nitindhar/kampr/adapters/UserPostsAdapter.java
+++ src/com/nitindhar/kampr/adapters/UserPostsAdapter.java
@@ -5,7 +5,6 @@ import java.util.List;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.nitindhar.kampr.R;
@@ -23,22 +22,6 @@ public class UserPostsAdapter<T> extends AbstractListAdapter<T> {
         convertView = getConvertView(convertView, R.layout.user_post_item);
 
         PostDecorator pd = (PostDecorator) objects.get(position);
-
-        ImageView postTypeIcon = (ImageView) getViewHandle(convertView,
-                R.id.user_post_item_type_icon);
-        if (pd.getPost().getPostType().equals("link")) {
-            postTypeIcon.setImageDrawable(context.getResources().getDrawable(
-                    R.drawable.list_link));
-        } else if (pd.getPost().getPostType().equals("snap")) {
-            postTypeIcon.setImageDrawable(context.getResources().getDrawable(
-                    R.drawable.list_snap));
-        } else if (pd.getPost().getPostType().equals("code")) {
-            postTypeIcon.setImageDrawable(context.getResources().getDrawable(
-                    R.drawable.list_code));
-        } else {
-            postTypeIcon.setImageDrawable(context.getResources().getDrawable(
-                    R.drawable.list_question));
-        }
 
         TextView postDate = (TextView) getViewHandle(convertView,
                 R.id.user_post_item_date);
